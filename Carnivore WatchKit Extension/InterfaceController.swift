b565e3fc05811c70e2e4664895aa67c27b9389f3
IJke <ijke@overscope.net>
2018-01-15 14:14:39 +0100

    Restore to starter project.

--- Carnivore WatchKit Extension/InterfaceController.swift
+++ Carnivore WatchKit Extension/InterfaceController.swift
@@ -9,23 +9,53 @@
 import WatchKit
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet var timer: WKInterfaceTimer!
+    @IBOutlet var timerButton: WKInterfaceButton!
+    @IBOutlet var weightPicker: WKInterfacePicker!
+    
+    var ounces = 16
+    var cookTemp = MeatTemperature.medium
+    var timerRunning = false
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
+        var weightItems: [WKPickerItem] = []
+        for number in 1...32 {
+            let item = WKPickerItem()
+            item.title = String(number)
+            weightItems.append(item)
+        }
+        weightPicker.setItems(weightItems)
+        weightPicker.setSelectedItemIndex(ounces - 1)
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    override func interfaceOffsetDidScrollToTop() {
+        print("User scrolled to top")
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    override func interfaceDidScrollToTop() {
+        print("User went to top by tapping status bar")
     }
-
+    
+    override func interfaceOffsetDidScrollToBottom() {
+        print("User scrolled to bottom")
+    }
+    
+    @IBAction func onTimerButton() {
+        if timerRunning {
+            timer.stop()
+            timerButton.setTitle("Start Timer")
+        } else {
+            let time = cookTemp.cookTimeForOunces(ounces)
+            timer.setDate(Date(timeIntervalSinceNow: time))
+            timer.start()
+            timerButton.setTitle("Stop Timer")
+        }
+        timerRunning = !timerRunning
+        scroll(to: timer, at: .top, animated: true)
+    }
 }
+
